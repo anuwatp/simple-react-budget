0fe0d2520f8b4fa3e9ce484be8240f24e4c622ee
Anuwat <anuwat94@hotmail.no>
2020-10-19 15:43:39 +0200

    Deploy 4rd attempt

---
+++ build/precache-manifest.17f14e281302332e7a6fb748fe917c4d.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7c884e94e34beccac29b82ee6f4623b6",
+    "url": "/index.html"
+  },
+  {
+    "revision": "5a37be83c493fc6416eb",
+    "url": "/static/css/2.d9ad5f5c.chunk.css"
+  },
+  {
+    "revision": "ace810c3de587e333101",
+    "url": "/static/css/main.3d0f88f6.chunk.css"
+  },
+  {
+    "revision": "5a37be83c493fc6416eb",
+    "url": "/static/js/2.2df72c22.chunk.js"
+  },
+  {
+    "revision": "8b81bc38395cca664a526c4e304a6603",
+    "url": "/static/js/2.2df72c22.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ace810c3de587e333101",
+    "url": "/static/js/main.ace58c09.chunk.js"
+  },
+  {
+    "revision": "3382d50b18e01dd65e1d",
+    "url": "/static/js/runtime-main.f90f7333.js"
+  }
+]);
